ede80c48c7061db9f4183e4da42526ae7a51efd3
Akinbola247 <112096641+Akinbola247@users.noreply.github.com>
2026-09-07 09:37:50 +0100

    Add adapters index.ts.

---
+++ packages/adapters/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./fixture-adapters.js";
+export * from "./stacks-api-adapter.js";
+export * from "./registry-provider.js";
+export * from "./stacks-read-only-client.js";
+export * from "./zest-mainnet-adapter.js";
+export * from "./bitflow-mainnet-adapter.js";
